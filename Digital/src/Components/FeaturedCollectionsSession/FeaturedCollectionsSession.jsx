d06511248f529f3856c37cfd8671059b2eea4cfe
JLucas78 <148010201+JLucas78@users.noreply.github.com>
2024-07-23 10:44:39 -0300

    Atualização do projeto - Pagina de visualização

--- Digital/src/Components/FeaturedCollectionsSession/FeaturedCollectionsSession.jsx
+++ Digital/src/Components/FeaturedCollectionsSession/FeaturedCollectionsSession.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import camiseta from "../../assets/camiseta.svg";
 import calca from "../../assets/calça.svg";
 import bone from "../../assets/hat.svg";
@@ -7,26 +7,47 @@ import tenis from "../../assets/tenis.svg";
 import "./FeaturedCollectionsSession.css";
 
 function FeaturedCollectionsSession() {
+    useEffect(() => {
+        const objects = document.querySelectorAll('.Session object');
+        
+        objects.forEach(object => {
+            object.addEventListener('load', () => {
+                const svgDoc = object.contentDocument;
+                if (svgDoc) {
+                    const paths = svgDoc.querySelectorAll('path');
+                    
+                    object.addEventListener('mouseover', () => {
+                        paths.forEach(path => path.setAttribute('fill', '#c92071'));
+                    });
+                    
+                    object.addEventListener('mouseout', () => {
+                        paths.forEach(path => path.setAttribute('fill', '#8f8f8f'));
+                    });
+                }
+            });
+        });
+    }, []);
+
     return (
         <div className="Session">
             <div className="camiseta">
-                <img src={camiseta} alt="Camiseta" className='camiseta'/>
-                <h6 style={{ position: 'relative', bottom: '0.5rem' }}>Camiseta</h6>
+                <object type="image/svg+xml" data={camiseta} className='camiseta'>Camiseta</object>
+                <h6 style={{ position: 'relative', top: '1.2rem' }}>Camiseta</h6>
             </div>
-            <div className="calca">
-                <img src={calca} alt="Calça"/>
+            <div className="calca" >
+                <object type="image/svg+xml" data={calca} style={{width: '5rem'}}>Calça</object>
                 <h6 style={{ position: 'relative', top: '0.3rem' }}>Calça</h6>
             </div>
             <div className="bone">
-                <img src={bone} alt="Boné" className='bone'/>
+                <object type="image/svg+xml" data={bone} className='bone' >Boné</object>
                 <h6 style={{ position: 'relative', top: '0.6rem' }}>Boné</h6>
             </div>
             <div className="headphone">
-                <img src={headphone} alt="Headphone" />
+                <object type="image/svg+xml" data={headphone} style={{width: '5rem'}}>Headphone</object>
                 <h6 style={{ position: 'relative', top: '0.3rem' }}>Headphone</h6>
             </div>
             <div className="tenis">
-                <img src={tenis} alt="Tênis" />
+                <object type="image/svg+xml" data={tenis} style={{width: '5rem' , fill: '#099000'}}>Tênis</object>
                 <h6 style={{ position: 'relative', top: '0.3rem' }}>Tênis</h6>
             </div>
         </div>
--- Digital/src/Pages/ProductListingPage/ProductListingPage.jsx
+++ Digital/src/Pages/ProductListingPage/ProductListingPage.jsx
@@ -3,17 +3,37 @@ import ProductCard from '../../Components/ProductCard/ProductCard';
 
 const ProductListingPage = () => {
   return (
-    <div className='totalCard'>
-      <div className='cardContent'>
-        <ProductCard width={250} height={270} />
-        <ProductCard width={250} height={270} />
-        <ProductCard width={250} height={270} />
-        <ProductCard width={250} height={270} />
-        <ProductCard width={250} height={270} />
-        <ProductCard width={250} height={270} />
-        <ProductCard width={250} height={270} />
-        <ProductCard width={250} height={270} />
-      </div>
+
+    
+    <div
+      className='ListingPage-CardContent'
+      style={{
+        width: '850px',
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '1.5rem',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        position: 'relative',
+        left: '10rem'
+      }}
+    >
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+      <ProductCard width={250} height={270} />
+    
     </div>
   );
 };
